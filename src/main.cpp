8f65c15a9ea3b4a2f44ebbacfa1546dde9c36677
LoadChange <soocto@gmail.com>
2022-01-27 18:57:26 +0800

    bugfix

--- src/main.cpp
+++ src/main.cpp
@@ -41,13 +41,74 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN, NEO_GRB + NEO_KHZ400);
 RTC_DS3231 rtc;
 char t[32];
 volatile bool tick;
-volatile bool calibration;
 
 void inline timer0_ISR(void)
 {
     tick = true;
     // reprime the timer
     timer0_write(ESP.getCycleCount() + 80000000L); // 80MHz == 1sec
+}
+
+void show(uint8_t offset, int t)
+{
+    // offset 0 for seconds
+    // offset 8 for minutes
+    // offset 12 for hours
+    uint8_t tens = t / 10;
+    uint8_t ones = t % 10;
+
+    // ones go up
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        if (bitRead(ones, i))
+        {
+            strip.setPixelColor(offset + 7 - i, strip.Color(10, 22, 22));
+        }
+    }
+
+    // tens go down
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        if (bitRead(tens, i))
+        {
+            strip.setPixelColor(offset + i, strip.Color(10, 22, 22));
+        }
+    }
+}
+
+void show_test_led(int count)
+{
+    strip.clear();
+    show(8, count);
+    strip.show();
+}
+
+void init_network()
+{
+    Serial.print("init network...");
+    wifiManager.setTimeout(600);
+    bool connect = wifiManager.autoConnect();
+    Serial.println("Connecting to wifi");
+    if (!connect)
+    {
+        delay(1000);
+    }
+    else
+    {
+        show_test_led(4);
+        timeClient.update();
+        if (timeClient.getEpochTime() < 1643308074)
+        {
+            init_network();
+        }
+        DateTime ntpTime = DateTime(timeClient.getEpochTime());
+        Serial.print(timeClient.getEpochTime());
+        Serial.print(F(" => NTP DateTime: "));
+        Serial.println(ntpTime.toString("YYYY/MM/DD hh:mm:ss"));
+        Serial.println("Set current time...");
+        rtc.adjust(ntpTime);
+    }
+    show_test_led(8);
 }
 
 void setup()
@@ -78,36 +139,9 @@ void setup()
     timer0_attachInterrupt(timer0_ISR);
     timer0_write(ESP.getCycleCount() + 80000000L); // 80MHZ / 1sec
     interrupts();
+    init_network();
     // prime our first tick
     tick = true;
-    calibration = false;
-}
-
-void show(uint8_t offset, int t)
-{
-    // offset 0 for seconds
-    // offset 8 for minutes
-    // offset 12 for hours
-    uint8_t tens = t / 10;
-    uint8_t ones = t % 10;
-
-    // ones go up
-    for (uint8_t i = 0; i < 4; i++)
-    {
-        if (bitRead(ones, i))
-        {
-            strip.setPixelColor(offset + 7 - i, strip.Color(0, 0, 255));
-        }
-    }
-
-    // tens go down
-    for (uint8_t i = 0; i < 4; i++)
-    {
-        if (bitRead(tens, i))
-        {
-            strip.setPixelColor(offset + i, strip.Color(0, 0, 255));
-        }
-    }
 }
 
 void loop()
@@ -115,65 +149,24 @@ void loop()
     // update on every tick
     if (tick)
     {
+        Serial.print(F("Temperature: "));
+        Serial.print(rtc.getTemperature());
+        Serial.print(F("C°, DS3231 DateTime: "));
+        Serial.println(rtc.now().toString("YYYY/MM/DD hh:mm:ss"));
 
         // update timeclient
         // timeClient.update();
         // debug output to serial
         // Serial.println(timeClient.getFormattedTime());
 
-        // ------------------
-        DateTime now = rtc.now();
-
-        Serial.print(F("Temperature: "));
-        Serial.print(rtc.getTemperature());
-        Serial.print(F("C°, DS3231 DateTime: "));
-        Serial.println(now.toString("YYYY/MM/DD hh:mm:ss"));
-
-        // TODO: DEBUG..
-        if ((now.second() % 10 == 0) || !calibration)
-        {
-            int count = 0;
-            calibration = false;
-            while (!calibration)
-            {
-                if (count > 30)
-                {
-                    calibration = true;
-                }
-                count += 1;
-                calibration = wifiManager.autoConnect();
-                Serial.println("Connecting to wifi");
-                delay(1000);
-            }
-            timeClient.update();
-            DateTime ntpTime = DateTime(timeClient.getEpochTime());
-
-            Serial.print(timeClient.getEpochTime());
-            Serial.print(F(" => NTP DateTime: "));
-            Serial.println(ntpTime.toString("YYYY/MM/DD hh:mm:ss"));
-
-            if (ntpTime.unixtime() > now.unixtime())
-            {
-                Serial.println("Set current time...");
-                rtc.adjust(ntpTime);
-            }
-            else
-            {
-                Serial.print(F("Cannot use past time: "));
-                Serial.print(now.unixtime());
-                Serial.print(F(" > "));
-                Serial.println(ntpTime.unixtime());
-            }
-        }
-
-        // ------------------
-
         // clear the strip
         strip.clear();
 
-        show(16, now.second());
-        show(8, now.minute());
-        show(0, now.hour());
+        Serial.println(rtc.now().second());
+
+        show(16, rtc.now().second());
+        show(8, rtc.now().minute());
+        show(0, rtc.now().hour());
 
         // push to the strip
         strip.show();
